0d543837fa03c4b4ab38f63038d09f1bb1ed475d
Alexandreifage <gavilletalexandre@gmail.com>
2020-09-23 20:50:07 +0200

    23.9

--- src/com/geometrie/etude/figure/Triangle.java
+++ src/com/geometrie/etude/figure/Triangle.java
@@ -1,5 +1,43 @@
 package com.geometrie.etude.figure;
 
-public class Triangle {
+public class Triangle extends Polygone {
+	private static final int NBR_COTES = 2;
+	
+	
+	private double hauteur;
+	private double base;
+	
+	//constructeur:
+	public Triangle(double base, double hauteur) {
+		super(NBR_COTES);
+		this.hauteur = hauteur;
+		this.base = base;
+		
+	}
+	
+	// methode
+	@Override
+	public double calculerSurface() {
+		return base * hauteur / 2;
+	}
+	
+	
+	// getters and setters
+	public double getHauteur() {
+		return hauteur;
+	}
+
+	public void setHauteur(double hauteur) {
+		this.hauteur = hauteur;
+	}
+
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+	
 
 }
--- src/com/geometrie/etude/test/TestFigure.java
+++ src/com/geometrie/etude/test/TestFigure.java
@@ -1,7 +1,9 @@
 package com.geometrie.etude.test;
 
 import com.geometrie.etude.figure.Carre;
+import com.geometrie.etude.figure.Cercle;
 import com.geometrie.etude.figure.Figure;
+import com.geometrie.etude.figure.Rectangle;
 import com.geometrie.etude.figure.Triangle;
 
 public final class TestFigure {
@@ -9,9 +11,44 @@ public final class TestFigure {
 	public static void main(String[] args) {
 		
 		Figure f = new Triangle(23, 44);
-		f = new Carre(25);
+		afficherSurface(f);	
+		
+		f = new Carre(10);
+
+		afficherSurface(f);	
+		
+		Triangle t = new Triangle (25, 32);
+		t.calculerSurface();
+		
+	
+		Rectangle r = new Rectangle(11, 22);
+		afficherSurface(r);
+		
+		double Aire = t.calculerSurface();
+		System.out.println("La surface du triangle est de  " + Aire);
+		
+		Carre c = new Carre (25);
+		c.calculerSurface();
+		
+		double Aire2 = c.calculerSurface();
+		System.out.println("La surface du carré est de  " + Aire2);
+		
+		Cercle ce = new Cercle (25);
+		ce.calculerSurface();
+		
+		double Aire3 = ce.calculerSurface();
+		System.out.println("La surface du cercle est de  " + Aire3);
 		
 		
+	}
+	
+	public static void afficherSurface(Figure figure) {
+		
+		if(figure instanceof Triangle) {
+			double base = ((Triangle)figure).getBase();
+			System.out.println("la base ti triangle est de: " + base);
+		}
+		System.out.println("surface " + figure.calculerSurface());
 	}
 
 }
